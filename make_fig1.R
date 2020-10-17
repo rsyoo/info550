f6dfd4aa905d680eeb8e072eda8792b9ef9b79c6
Robin Yoo <sodahm.robin.yoo@emory.edu>
2020-10-16 18:28:16 -0700

    added comments

--- make_fig1.R
+++ make_fig1.R
@@ -3,8 +3,10 @@
 # read in data
 load("birth_cleaned.Rdata")
 
+# save scatterplot as png file
 png("fig1.png")
 
+# make scatterplot
 plot(birth$BIRTHWEIGHT, birth$AGE_M,
      xlab = "Birthweight (g)",
      ylab = "Maternal age (yrs)",
